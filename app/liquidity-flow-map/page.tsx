9a0ed9f45d372797e83da1315b63122a55982f4e
marioandres97 <127124964+marioandres97@users.noreply.github.com>
2025-10-17 22:52:52 +0200

    Create main page for Liquidity Flow Map

---
+++ app/liquidity-flow-map/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LiquidityFlowMapPage = () => {
+  return (
+    <div className="flex flex-col">
+      {/* Coin selector, timeframe selector, live indicator, etc. */}
+      <h1>Liquidity Flow Map</h1>
+      {/* Components go here */}
+    </div>
+  );
+};
+
+export default LiquidityFlowMapPage;
